c08c15c9f58743b260d73cf961f6c735ca84ad97
Mike <mb425@cam.ac.uk>
2021-08-03 13:37:17 +0100

    Added a travel simple scheduler

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<functional>
 #include<vector>
 #include<set>
 #include<assert.h>
@@ -15,30 +16,64 @@ public:
     void remove(agent* a){
         occupants.erase(a);
     }
+    void update(){}
     void show(bool);
+};
+class placeChanger{
+    place* origin,*destination;
+public:
+    placeChanger(){origin=nullptr;destination=nullptr;}
+    placeChanger(agent* a, place* o,place* d):origin(o),destination(d){ 
+        origin->remove(a);
+        destination->add(a);
+    }
+    bool update(){return false;}
+    //auto n=[&](agent* a){a->atHome();};
+    //void operator()(agent* a){a->atHome();}
 };
 class agent{
 public:
     int ID;
+    place* currentPlace;
     place* home;
     place* work;
     //transport vehicles are places, albeit moveable!
     place* bus;
+    
+    std::vector<placeChanger*> p;
+    int schedule;
     agent(){
         home=0;
         work=0;
         bus=0;
     }
-    void doStuff(){
-        atHome();
-        enterTransport(home,bus);//load people into busstop rather than direct into bus?
-        inTransit();//trip chaining here?
-        leaveTransport(bus,work);
-        atWork();
-        enterTransport(work,bus);
-        inTransit();
-        leaveTransport(bus,home);
-        atHome();
+    void initTravelSchedule(){
+        p.push_back(new placeChanger(this,home,bus));//load people into busstop (transportHub) rather than direct into bus - here they wait
+        p.push_back(new placeChanger(this,bus,work));
+        p.push_back(new placeChanger(this,work,bus));
+        p.push_back(new placeChanger(this,bus,home));        
+        schedule=0;
+    }
+    void update(){
+        //Use the base travel schedule - initialised at home for everyone
+
+        //auto k=[&](){atHome();};
+        //k();
+        if (ID==0)std::cout<<schedule<<std::endl;
+        p[schedule]->update();
+        schedule++;
+        schedule=schedule % p.size();
+
+        //enterTransportHub();
+        //enterTransport(home,bus);
+        //inTransit();//trip chaining here? how to handle trips across multiple transport hubs? how to do schools (do parents load up childer?) and shops?
+        //leaveTransport(bus,work);
+        //atWork();//this could involve travelling too - e.g. if delivery driver 
+        //enterTransport(work,bus);
+        //inTransit();
+        //leaveTransport(bus,home);
+        //exitTransportHub(); go from busstop (or station etc.) to home
+        //atHome();//people might be at some other location overnight - e.g. holiday or trucker in their cab - but home can have special properties (e.g. food storage, places where I keep my stuff)
     }
     void enterTransport(place* origin,place* transportMode){
         origin->remove(this);
@@ -49,6 +84,9 @@ public:
         destination->add(this);
     }
     void atHome(){
+        //stigmergic disease at all locations? - how to update places consistently (and simply) with agents?
+        //breathInto(place)? (what about surfaces?)
+        std::cout<<"at Home"<<std::endl;
     }
     void atWork(){
     }
@@ -65,6 +103,7 @@ public:
     void setTransport(place* p){
         bus=p;
     }
+
 };
 void place::show(bool listAll=false){
     std::cout<<"Place ID "<<ID<<" has "<<occupants.size()<<" occupants"<<std::endl;
@@ -75,10 +114,23 @@ void place::show(bool listAll=false){
         }
     }
 }
+
+//    template <typename F>
+//void ask(std::set<agent*> ps,F f){
+//    for (auto& p: ps) f(*p);
+//}
+    template <typename F>
+void ask(agent* ps,F f){
+    f(ps);
+}
 int main(int argc, char **argv) {
+//agent* w=new agent();
+//auto n=[&](agent* a){a->atHome();};
+//ask(w,n);
     std::vector<agent*> agents;
     std::vector<place*> places;
     int nAgents=60000;
+    int nSteps=10;
     //create homes
     for (int i=0;i<nAgents/3;i++){
         place* p=new place();
@@ -100,8 +152,8 @@ int main(int argc, char **argv) {
     }
     //allocate 10 agents per workplace
     for (int i=0;i<agents.size();i++){
-        assert(places[i/10+200]!=0);
-        agents[i]->setWork(places[i/10+200]);
+        assert(places[i/10+nAgents/3]!=0);
+        agents[i]->setWork(places[i/10+nAgents/3]);
     }
     //create buses
     for (int i=nAgents/3+nAgents/10;i<nAgents/3+nAgents/10+nAgents/30;i++){
@@ -111,17 +163,24 @@ int main(int argc, char **argv) {
     }
     //allocate 30 agents per bus
     for (int i=0;i<agents.size();i++){
-        assert(places[i/30+260]!=0);
-        agents[i]->setTransport(places[i/30+260]);
+        assert(places[i/30+nAgents/3+nAgents/10]!=0);
+        agents[i]->setTransport(places[i/30+nAgents/3+nAgents/10]);
     }
-    //move between the two
-    for (int step=0;step<10;step++){
+    for (int i=0;i<agents.size();i++){
+        agents[i]->initTravelSchedule();
+    }
+    std::cout<<"Built "<<agents.size()<<" agents and "<<places.size()<<" places."<<std::endl;
+    //move between the places
+    for (int step=0;step<nSteps;step++){
         for (int i=0;i<agents.size();i++){
-            agents[i]->doStuff();
+            agents[i]->update();
+        }
+        for (int i=0;i<places.size();i++){
+            places[i]->update();
         }
     }
     for (int i=0;i<places.size();i++){
-        places[i]->show();
+        //places[i]->show();
     }
     return 0;
 }
