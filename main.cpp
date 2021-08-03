3aee725611e8b355c358e71b708e2152e4af256e
Mike <mb425@cam.ac.uk>
2021-08-03 15:06:55 +0100

    Added a simple disease

--- main.cpp
+++ main.cpp
@@ -7,16 +7,19 @@ class agent;
 class place{
 public:
     int ID;
+    float contaminationLevel,decrement;
     //unique list of current people in this place
     std::set<agent*> occupants;
-    place(){ID=0;}
+    place(){ID=0;contaminationLevel=0.;decrement=0.9;}
     void add(agent* a){
         occupants.insert(a);
     }
     void remove(agent* a){
         occupants.erase(a);
     }
-    void update(){}
+    void increaseContamination(float amount){contaminationLevel+=amount;}
+    float getContaminationLevel(){return contaminationLevel;}
+    void update(){contaminationLevel*=decrement;}
     void show(bool);
 };
 class placeChanger{
@@ -27,9 +30,7 @@ public:
         origin->remove(a);
         destination->add(a);
     }
-    bool update(){return false;}
-    //auto n=[&](agent* a){a->atHome();};
-    //void operator()(agent* a){a->atHome();}
+    place* update(){return destination;}
 };
 class agent{
 public:
@@ -38,42 +39,36 @@ public:
     place* home;
     place* work;
     //transport vehicles are places, albeit moveable!
-    place* bus;
+    place* vehicle;
     
-    std::vector<placeChanger*> p;
+    std::vector<placeChanger*> travel;
     int schedule;
+    bool diseased;
     agent(){
         home=0;
         work=0;
-        bus=0;
+        vehicle=0;
+        diseased=false;
     }
     void initTravelSchedule(){
-        p.push_back(new placeChanger(this,home,bus));//load people into busstop (transportHub) rather than direct into bus - here they wait
-        p.push_back(new placeChanger(this,bus,work));
-        p.push_back(new placeChanger(this,work,bus));
-        p.push_back(new placeChanger(this,bus,home));        
+        travel.push_back(new placeChanger(this,home,vehicle));//load people into busstop (or transportHub) rather than direct into bus? - here they would wait
+        travel.push_back(new placeChanger(this,vehicle,work));//trip chaining? how to handle trips across multiple transport hubs? how to do schools (do parents load up childer?) and shops?
+        travel.push_back(new placeChanger(this,work,vehicle));
+        travel.push_back(new placeChanger(this,vehicle,home));        
         schedule=0;
     }
     void update(){
         //Use the base travel schedule - initialised at home for everyone
-
-        //auto k=[&](){atHome();};
-        //k();
         if (ID==0)std::cout<<schedule<<std::endl;
-        p[schedule]->update();
+        currentPlace=travel[schedule]->update();
         schedule++;
-        schedule=schedule % p.size();
-
-        //enterTransportHub();
-        //enterTransport(home,bus);
-        //inTransit();//trip chaining here? how to handle trips across multiple transport hubs? how to do schools (do parents load up childer?) and shops?
-        //leaveTransport(bus,work);
-        //atWork();//this could involve travelling too - e.g. if delivery driver 
-        //enterTransport(work,bus);
-        //inTransit();
-        //leaveTransport(bus,home);
-        //exitTransportHub(); go from busstop (or station etc.) to home
-        //atHome();//people might be at some other location overnight - e.g. holiday or trucker in their cab - but home can have special properties (e.g. food storage, places where I keep my stuff)
+        schedule=schedule % travel.size();
+        if (diseased)currentPlace->increaseContamination(0.1);
+        if (currentPlace->getContaminationLevel()>0.4)diseased=true;
+        if (currentPlace==home)atHome();//people might be at some other location overnight - e.g. holiday, or trucker in their cab - but home can have special properties (e.g. food storage, places where I keep my stuff)
+        if (currentPlace==vehicle)inTransit();
+        if (currentPlace==work)atWork();//this could involve travelling too - e.g. if delivery driver 
+        
     }
     void enterTransport(place* origin,place* transportMode){
         origin->remove(this);
@@ -86,22 +81,26 @@ public:
     void atHome(){
         //stigmergic disease at all locations? - how to update places consistently (and simply) with agents?
         //breathInto(place)? (what about surfaces?)
-        std::cout<<"at Home"<<std::endl;
+        if (ID==0)std::cout<<"at Home"<<std::endl;
+        if (ID==1)std::cout<<"cough! at place ID "<<currentPlace->ID<<": "<<home->getContaminationLevel()<<std::endl;
     }
     void atWork(){
+        if (ID==0)std::cout<<"at Work"<<std::endl;
     }
     void inTransit(){
+        if (ID==0)std::cout<<"on Bus"<<std::endl;
     }
     void setHome(place* p){
         home=p;
         //start all agents at home
         home->add(this);
+        currentPlace=home;
     }
     void setWork(place* p){
         work=p;
     }
     void setTransport(place* p){
-        bus=p;
+        vehicle=p;
     }
 
 };
@@ -115,22 +114,13 @@ void place::show(bool listAll=false){
     }
 }
 
-//    template <typename F>
-//void ask(std::set<agent*> ps,F f){
-//    for (auto& p: ps) f(*p);
-//}
-    template <typename F>
-void ask(agent* ps,F f){
-    f(ps);
-}
+
 int main(int argc, char **argv) {
-//agent* w=new agent();
-//auto n=[&](agent* a){a->atHome();};
-//ask(w,n);
+
     std::vector<agent*> agents;
     std::vector<place*> places;
-    int nAgents=60000;
-    int nSteps=10;
+    int nAgents=60;
+    int nSteps=1000;
     //create homes
     for (int i=0;i<nAgents/3;i++){
         place* p=new place();
@@ -170,17 +160,37 @@ int main(int argc, char **argv) {
         agents[i]->initTravelSchedule();
     }
     std::cout<<"Built "<<agents.size()<<" agents and "<<places.size()<<" places."<<std::endl;
+    agents[0]->diseased=true;
     //move between the places
     for (int step=0;step<nSteps;step++){
+        int ill=0;
         for (int i=0;i<agents.size();i++){
             agents[i]->update();
+            if (agents[i]->diseased)ill++;
         }
         for (int i=0;i<places.size();i++){
             places[i]->update();
         }
+        std::cout<<"Infected "<<ill<<std::endl;
     }
     for (int i=0;i<places.size();i++){
         //places[i]->show();
     }
     return 0;
 }
+//reminders of how to do some function cally things
+        //auto k=[&](){atHome();};
+        //k();
+//agent* w=new agent();
+//auto n=[&](agent* a){a->atHome();};
+//    template <typename F>
+//void ask(std::set<agent*> ps,F f){
+//    for (auto& p: ps) f(*p);
+//}
+//    template <typename F>
+//void ask(agent* ps,F f){
+//    f(ps);
+//}
+//ask(w,n);
+    //auto n=[&](agent* a){a->atHome();};
+    //void operator()(agent* a){a->atHome();}
