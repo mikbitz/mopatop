de32f9a82abfd563be51de57ba8a76fbcc77ff5e
Mike <mb425@cam.ac.uk>
2021-08-17 16:46:04 +0100

    added header gurads to parameters.h

--- parameters.h
+++ parameters.h
@@ -1,3 +1,29 @@
+/* A program to model agents moving between places
+    Copyright (C) 2021  Mike Bithell
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    */
+/**
+ * @file parameters.h 
+ * @brief File containing the definition of the parameterSettings class
+ * 
+ * @author Mike Bithell
+ * @date 17/08/2021
+ **/
+#ifndef PARAMETERS_H_INCLUDED
+#define PARAMETERS_H_INCLUDED
+
 /**
  * @brief ParameterSettings is a class designed to hold all the parameters for the model
  * @details At present these are hard coded here, but more usefully these could be delegated to an input file.
@@ -130,3 +156,4 @@ public:
         return parameters[s];
     }
 };
+#endif // PARAMETERS_H_INCLUDED
