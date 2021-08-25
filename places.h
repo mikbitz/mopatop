02b70c1cd9e6a943262b141794ff9ebd837d6611
Mike <mb425@cam.ac.uk>
2021-08-25 11:52:54 +0100

    separated places into their own header file

---
+++ places.h
@@ -0,0 +1,129 @@
+/* A program to model agents moving between places
+    Copyright (C) 2021  Mike Bithell
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    */
+/**
+ * @file places.h 
+ * @brief File containing the deinfition of the places class
+ * @details Note that some of teh method implementations (that depend on the agent class) are currently in main.cpp
+ * 
+ * @author Mike Bithell
+ * @date 17/08/2021
+ **/
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+//Forward declaration of agent as they are needed in place class
+//This is a C++ idiom where two classes refer to each other, so neither can be cleanly set up first
+//This declaration allows the place class to know that agents exist, but not their structure
+#ifndef PLACES_H_INCLUDED
+#define PLACES_H_INCLUDED
+
+class agent;
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+/**
+ * @brief Places can have a list of occupants, and store disease contamination
+ * @details At the moment places are disjoint from each other - agents travel between them on a schedule, but don't interact with other not in the same place. \n
+ * At present the interaction is indirect through contamination that agents in a given place can leave there, if they are carying a disease. \n
+ * Places incliude mobile entities such as buses - so the schedule for movement between places needs to include explicit getting into vehicles, and a specifcation of time spent there, \n
+ * as contamination level should depend on the duration of stay in a given place.
+*/
+class place{
+
+    /** Unique identifier for a place - should be able to go up to about 4e9 */
+    unsigned long ID;
+    /** An arbitrary number giving how infectious a given place currently might be - needs calibration to get a suitable per-unit-time value. \n 
+     One might expect it to vary with the size of a given location
+     */
+    double contaminationLevel;
+    /** Rate of decrease of contamination - per time step (exponential) \n */
+    double fractionalDecrement;
+    /** @brief This flag is used to clear out any contamination at the start of every timestep, if required
+     * @details for example, if one wants the contamination level to be just proportional to the current number\n
+     * of agents in a place at the opint where agents test for infection, set this to true.  
+     */
+    bool cleanEveryStep;
+    /** unique list of current people in this place - intended for direct agent-agent interaction \n
+     * For the current disease model this is not needed, as the agents need only know where they are to contaminate a place \n
+     * currently this is not used...seems to add about 20% to memory requirement if populated. \n
+     * A std::set is unique - so the same agent can be added many times but will only apear once.\n
+     * The set uses the pointer to a given agent as the key, so its easy to insert or remove arbitrary agents */
+    std::set<agent*> occupants;
+public:
+    /** @brief set up the place. 
+     *  @details Assumed initially clean. The decrement value might very with place type and ventialtion level...\n
+        but here is set to a fixed number*/
+    place(){
+        ID=0;
+        contaminationLevel=0.;
+        fractionalDecrement=0.1;
+        cleanEveryStep=false;
+    }
+    /** @brief set up the place. 
+     *  @param p parameter Settings read from the parameter file 
+     *  @details Assumed initially clean. The decrement value is here imported from the parameter settings */
+    place(parameterSettings p){
+        ID=0;
+        contaminationLevel=0.;
+        fractionalDecrement=p.get<double>("places.disease.simplistic.fractionalDecrement");
+        cleanEveryStep=p.get<bool>("places.cleanContamination");        
+    }
+    /** @brief set the place ID number
+     *  @details care shoudl be taken that ths value set here is unique! */
+    void setID(long i){
+        ID=i;
+    }
+    /**Add an agent to the list currently here 
+     @param a a pointer to the agent to be added */
+    void add(agent* a){
+        occupants.insert(a);
+    }
+    /**Remove an agent from the list currently here 
+     @param a a pointer to the agent to be removed */
+    void remove(agent* a){
+        occupants.erase(a);
+    }
+    /** A function to allow agents (or any other thing that points at this place) to add contamination that spreads disease
+     *  Essentially a proxy for droplets in the air or surface contamination \n
+     *  Agents that are infected will increase this level while this is their currentPlace , offsetting the decrease in \ref update */
+    void increaseContamination(double amount){
+        contaminationLevel+=amount;
+    }
+    /** A function to allow agents (or any other thing that points at this place) to completely clean up the contamination in a given place.
+     * The level gets reset to zero
+     * */
+    void cleanContamination(){
+        contaminationLevel=0.;
+    }
+    /** Get the current level of contamination here
+     *@return Floating point value of current contamination level. */
+    float getContaminationLevel(){
+        return contaminationLevel;
+    }
+    /** @brief The contamination in each place decays exponentially, or is reset to zero
+     * @details. This function should be called every (uniform) time step \n
+     *  This way places without any currently infected agents gradually lose their infectiveness, or else if \n
+     *  \ref cleanEveryStep is set, the place has all contamination removed - useful if contamination shoudl only be present\n
+     *  as long as agents are present, and amount should be directly given by the number of agents.
+     * */
+    void update(){
+        if (cleanEveryStep)cleanContamination();
+        else contaminationLevel*=exp(-fractionalDecrement);
+    }
+    /** Function to show the current status of a place - use with caution if there are many thousands of places! */
+    void show(bool);//defined below once agents are defined
+    //Because of the forward declaration of class agent, the full definition of this function has to wait until after the agent class is completed
+};
+#endif // PLACES_H_INCLUDED
