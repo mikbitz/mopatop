8a0b84df11e10d1d676f91abc873d281b60a7206
Mike <mb425@cam.ac.uk>
2021-08-31 09:50:34 +0100

    completed class for place testing

--- tests/testing.cpp
+++ tests/testing.cpp
@@ -1,3 +1,4 @@
+//compile with g++ -std=c++17 -lcppunit ../timestep.cpp testing.cpp
 #include <iostream>
 #include <string>
 #include <cppunit/TestCase.h>
@@ -8,6 +9,8 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/TextTestProgressListener.h>
 #include "../places.h"
+#include "../timestep.h"
+#include "../agent.h"
 #include<math.h>
 //fixtures allow setting up and tearing down of objects
 class placeTest : public CppUnit::TestFixture  {
@@ -29,6 +32,8 @@ public:
     CPPUNIT_TEST( testDefaultConstructor );
     CPPUNIT_TEST( testID );
     CPPUNIT_TEST( testContamination );
+    CPPUNIT_TEST( testUpdate );
+    CPPUNIT_TEST( testAgents );
     CPPUNIT_TEST_SUITE_END();
     //define tests
     void testDefaultConstructor()
@@ -39,13 +44,17 @@ public:
         CPPUNIT_ASSERT(std::abs(p->getContaminationLevel())<10*std::numeric_limits<double>::min());
         CPPUNIT_ASSERT(0.1-std::abs(p->getFractionalDecrement())<10*std::numeric_limits<double>::min());
         CPPUNIT_ASSERT(!p->getCleanEveryStep());
+        p->setCleanEveryStep();
+        CPPUNIT_ASSERT(p->getCleanEveryStep());
+        p->unsetCleanEveryStep();
+        CPPUNIT_ASSERT(!p->getCleanEveryStep());
     }
     void testID()
     {
         //default ID is zero
         CPPUNIT_ASSERT(0==p->getID());
-        p->setID(1);
-        CPPUNIT_ASSERT(1==p->getID());
+        p->setID(73);
+        CPPUNIT_ASSERT(73==p->getID());
     }
     void testContamination()
     {
@@ -60,6 +69,51 @@ public:
         //negatives get reset to zero.
         p->increaseContamination(-0.1);
         CPPUNIT_ASSERT(std::abs(p->getContaminationLevel())<10*std::numeric_limits<double>::min());
+    }
+    void testUpdate()
+    {
+        p->increaseContamination(0.1);
+        //make sure value is close to eseentially zero - because we are using doubles, it might not be exaclty equal
+        p->setCleanEveryStep();
+        p->update();
+        CPPUNIT_ASSERT(std::abs(p->getContaminationLevel())<10*std::numeric_limits<double>::min());
+        p->unsetCleanEveryStep();
+        p->increaseContamination(0.1);
+        p->setFractionalDecrement(0.0);
+        p->update();
+        CPPUNIT_ASSERT(0.1-std::abs(p->getContaminationLevel())<10*std::numeric_limits<double>::min());
+        p->setFractionalDecrement(0.5);
+        p->update();
+        //timestep is 1 hour by default.
+        CPPUNIT_ASSERT(0.1*exp(-0.5)-std::abs(p->getContaminationLevel())<10*std::numeric_limits<double>::min());
+        //set timestep to half an hour
+        timeStep::setdeltaT(0.5*timeStep::hour());
+        p->update();
+        CPPUNIT_ASSERT(0.1*exp(-0.5)*exp(-0.5*0.5)-std::abs(p->getContaminationLevel())<10*std::numeric_limits<double>::min());
+    }
+    void testAgents()
+    {
+        agent* a=new agent();
+        p->add(a);
+        CPPUNIT_ASSERT(p->getNumberOfOccupants()==1);
+        p->add(a);
+        //agents are unique
+        CPPUNIT_ASSERT(p->getNumberOfOccupants()==1);
+        p->remove(a);
+        CPPUNIT_ASSERT(p->getNumberOfOccupants()==0);
+        agent* b=new agent();
+        p->add(a);
+        p->add(a);
+        p->add(b);
+        CPPUNIT_ASSERT(p->getNumberOfOccupants()==2);
+        p->remove(a);
+        CPPUNIT_ASSERT(p->getNumberOfOccupants()==1);
+        p->add(a);
+        a->setID(10);
+        b->setID(15);
+        p->show();
+        p->show(true);
+        
     }
 };
 
