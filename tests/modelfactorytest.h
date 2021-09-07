0ebaf070c2f2d35df8665de70a82b52d5f47bc24
Mike <mb425@cam.ac.uk>
2021-09-07 12:47:40 +0100

    testing model factory

--- tests/modelfactorytest.h
+++ tests/modelfactorytest.h
@@ -37,13 +37,55 @@ class modelFactoryTest : public CppUnit::TestFixture  {
 public:
     /** @brief automatically create a test suite to add tests to  */
     CPPUNIT_TEST_SUITE( modelFactoryTest );
-    /** @brief test the default constructor  */
-    CPPUNIT_TEST( testDefaultConstructor );
+    /** @brief test the factory selector method  */
+    CPPUNIT_TEST( testSelector );
+    CPPUNIT_TEST( testCreation );
     /** @brief end the test suite   */
     CPPUNIT_TEST_SUITE_END();
-    /** @brief make sure results from constructor are as expected */
-    void testDefaultConstructor()
+    /** @brief make sure the selector works */
+    void testSelector()
     {
+        modelFactory& F=modelFactorySelector::select("simpleOnePlace");
+        simpleOnePlaceFactory H;
+        CPPUNIT_ASSERT(typeid(H).name()==typeid(F).name());
+        //default model factory in parameterSettings is simpleMobileFactory;
+        parameterSettings pr;
+        simpleMobileFactory G;
+        modelFactory& I=modelFactorySelector::select(pr("model.type"));
+        CPPUNIT_ASSERT(typeid(I).name()==typeid(G).name());
+    }
+    /** @brief chekc the creation of agents and places looks OK */
+    void testCreation()
+    {
+        std::vector<agent*> agents;
+        std::vector<place*> places;
+        parameterSettings pr;
+        modelFactory& F=modelFactorySelector::select("simpleOnePlace");
+        F.createAgents(pr,agents,places);
+        CPPUNIT_ASSERT(agents.size()==600);
+        CPPUNIT_ASSERT(places.size()==1);
+        //agents all in the same place
+        for (auto& a:agents)CPPUNIT_ASSERT(a->getHome()==places[0]);
+        //work and home are the same
+        for (auto& a:agents)CPPUNIT_ASSERT(a->getWork()==places[0]);
+        //schedule is just stay home
+        for (int i=0;i<100;i++){
+            agents[81]->update();
+            CPPUNIT_ASSERT(agents[81]->getHome()==places[0]);
+        }
+        agents.clear();
+        places.clear();
+        modelFactory& G=modelFactorySelector::select("simpleMobile");
+        G.createAgents(pr,agents,places);
+        CPPUNIT_ASSERT(agents.size()==600);
+        CPPUNIT_ASSERT(places.size()==280);
+        for (auto& a:agents)CPPUNIT_ASSERT(a->getHome()!=a->getWork());
+        for (auto& a:agents)CPPUNIT_ASSERT(a->getHome()==a->getCurrentPlace());
+        //schedule is home for 14 hours then transport 1 hour then work
+        for (int i=0;i<17;i++){
+            agents[10]->update();
+        }
+        CPPUNIT_ASSERT(agents[10]->getWork()==agents[10]->getCurrentPlace());
     }
 };
 
