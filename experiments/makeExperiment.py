e27d4310a5f5836b7390a95ea8a33409810440c2
Mike <mb425@cam.ac.uk>
2021-09-24 16:19:10 +0100

    Experiment generators now have README and RunFiles directory

--- experiments/makeExperiment.py
+++ experiments/makeExperiment.py
@@ -1,55 +1,74 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  20 15:00:44 2021
-
-@author: Mike Bithell
-@brief Create csv files to define sets of values to be used in an experiment.
-@details Read in the file that lists all the experiments, then 
-create a new row for the new experiment, including default path and specification
-file name. Write this to the experiment database. Then create the experiment
-directories, and use the example experiment to create a default parameter file
-and specification file (which says how the parameters will be varied during the 
-experiment).
-"""
+##@file makeExperiment.py
+#@brief Create csv files to define sets of values to be used in an experiment.
+#@details Read in the file that lists all the experiments, then 
+#create a new row for the new experiment, including default path and specification
+#file name. Write this to the experiment database. Then create the experiment
+#directories, and use the example experiment to create a default parameter file
+#and specification file (which says how the parameters will be varied during the 
+#experiment).
+#@author: Mike Bithell
+#Created on Mon Sep  20 15:00:44 2021
 import pandas as pd
 import os
 
-#experiment name
-Name='endTimeTest'
-#which version of the model is intended
+##@brief experiment name
+#@details Change this value to the name of the new experiment to be created
+Name = 'stochasticVariation'
+##@brief change to reflect version of the model
+#@details its expected that each expereriment is intended to run with a particular model version\n
+#which will be recorded in the experiment database
 modelVersion='0.4'
-#A string briefly giving experiment purpose
-purpose='How does end of infection vary with R'
-#default name of the specification file
+##@brief A string briefly giving experiment purpose
+#@details Change this to describe the experiment. More detail can be added in the parameter file and experiment README file
+purpose='Testing how randomseeds and numerbs of agents affect the output'
+##@brief default name of the specification file
+#This will be the name in the experiment database
 spec='base_'+Name+'.csv'
-#directory from which to find the experiments sub-directory
+##@brief directory from which to find the experiments sub-directory
+#@details the path here is relative to the place from which this cide is run
 root="../"
-#directory containing experiment specifications and their output
+##@brief directory containing experiment specifications and their output
+#@details defaults to experiments directory under the root
 experimentDirectory='experiments/'
+##@brief store the while path for later use
 path=root+experimentDirectory
-#file with listing of known experiments
+##@brief set the name of the experiment database
+#@details By default jsut a csv file listing of known experiments, one per row
 databaseFile='ExperimentDatabase.csv'
-#read in the experiment database
+##@brief read in the database and store the contents
+#@details the values are stored in a pandas dataframe
 experiments=pd.read_csv(path+databaseFile)
-#constrcut new Experiment row
+##@brief construct new Experiment row
+#@details this is a dictionary, with keys equal to the dataframe column names 
 newxp={'Name':Name, 'modelVersion':modelVersion, 'baseDirectory':experimentDirectory+Name, 'specificationFile':spec, 'purpose':purpose}
-#add new row into new dataframe
+##@brief add new row into new dataframe
+#@details the row will be added at the end of the current set of experiments.\n
+#Note this is not checked for duplicate names!
+#Values will get overwritten into the original database file
 newE=experiments.append(newxp,ignore_index=True)
-#write dataframe into the database file
+#write dataframe into the original database file
 newE.to_csv(path+databaseFile,index=False)
 #create a directory fo the new experiment
 os.system("mkdir "+path+Name)
 #make directory to hold parameter files for the experiment
 os.system("mkdir "+path+Name+"/parameterSets")
+#make directory to hold informational messages from each model run
+os.system("mkdir "+path+Name+"/RunFiles")
 #copy in the example speficication file to the new name
 os.system("cp "+path+"example/baseExample.csv "+path+Name+"/"+spec)
 #copy in the example parameter file
 os.system("cp "+path+"example/exampleBaseParameters "+path+Name+"/"+Name+"BaseParameters")
-#open the new experiment specifcation
+#copy in the README
+os.system("cp "+path+"example/README "+path+Name+"/README")
+##@brief a dataframe to hold the new experiment
+#@details Use the default file copied in from the example, and modify for the new experiment
 thisExperiment=pd.read_csv(path+Name+"/"+spec)
-#correct the experiment name and the base parameter file name
+#correct the experiment name 
+#first value in the first row after the header row
 thisExperiment.iat[0,0]=Name
+#correct the base parameter file name, second value in the first row
 thisExperiment.iat[0,1]=Name+"BaseParameters"
 #write out the spec file
 thisExperiment.to_csv(path+Name+"/"+spec,index=False)
@@ -58,4 +77,5 @@ print("Experiment specification "+Name+" created in "+path+Name)
 print("This experiment has been added as a new row in "+path+databaseFile )
 print("Now edit "+path+Name+"/"+Name+"BaseParameters "+ "to create the default set up")
 print("Then edit "+path+Name+"/"+spec+" to change the parameters")
+print("Also correct the model description in "+path+Name+"/README")
 print("Then you can run the experiment (or create a parameter file set) using "+path+"runExperiment.py"  )
--- experiments/runExperiment.py
+++ experiments/runExperiment.py
@@ -1,43 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  7 16:23:44 2021
+##@file runExperiment.py
+#@brief Use csv files to define sets of values to be used in an experiment.
+#@details Read in the file that lists all the experiments, then 
+#select on experiment to run, and use the base model parameter file as specified
+#for the experiment to write out a set of parameter files to cover the parameter
+#space. The expectation is that all parameters to be changed will be given in
+#the header row of the experiment specification file, starting at column 2
+#(columns 0 and 1 reserved for experiment name and parameter file name), with one
+#row for every parameter set to be used (every parameter to be changed must
+#have an entry in every row). This script can either directly run the model now
+#(given the name of an executable), or else store parameter files for later
+#(useful for hpc, for example). Note that all the paths for the experiment to 
+#be run need to be set up ahead of time, consistent with the base parameter file.
+#@author: Mike Bithell
+#Created on Tue Sep  7 16:23:44 2021
 
-@author: Mike Bithell
-@brief Use csv files to define sets of values to be used in an experiment.
-@details Read in the file that lists all the experiments, then 
-select on experiment to run, and use the base model parameter file as specified
-for the experiment to write out a set of parameter files to cover the parameter
-space. The expectation is that all parameters to be changed will be given in
-the header row of the experiment specification file, starting at column 2
-(columns 0 and 1 reserved for experiment name and parameter file name), with one
-row for every parameter set to be used (every parameter to be changed must
-have an entry in every row). This script can either directly run the model now
-(given the name of an executable), or else store parameter files for later
-(useful for hpc, for example). Note that all the paths for the experiment to 
-be run need to be set up ahead of time, consistent with the base parameter file.
-"""
 import pandas as pd
 import os
 import sys
 from datetime import datetime
 
-#Name of the experiment of the experiment to run
-experimentName='endTimeTest'
-#if set to True, run the model now! otherwise output a set of parameterFiles
+##Name of the experiment of the experiment to run
+experimentName='stochasticVariation'
+##if set to True, run the model now! otherwise output a set of parameterFiles
 directRun=True
-#needed if running the model now - the name of the executable to run
+##needed if running the model now - the name of the executable to run
 #assumed to live in root directory as below
 execname='agentModel'
-#directory from which to find the experiments sub-directory
+##directory from which to find the experiments sub-directory
 root="../"
-#file with listing of known experiments
+##file with listing of known experiments
 databaseFile='ExperimentDatabase.csv'
-#directory containing experiment specifications and their output
+##directory containing experiment specifications and their output
 experimentDirectory='experiments/'
-#read in the experiment database
+##read in the experiment database
 experiments=pd.read_csv(root+experimentDirectory+databaseFile)
-#row of the experiment database giving the experiment to run
+##row of the experiment database giving the experiment to run
 expE=experiments.index[experiments['Name']==experimentName]
 #check experiment exists
 if (len(expE)==0):
@@ -45,25 +44,29 @@ if (len(expE)==0):
 #extract the integer value from the list - there should only be one!
 if (len(expE)>1):
    sys.exit("Experiment "+experimentName+" occurs multiple times in database file "+root+experimentDirectory+databaseFile)
+##pick out the experiment
 experimentToRun=expE[0]
-#path to the directory for this experiment
+##path to the directory for this experiment
 path=root+experiments['baseDirectory'][experimentToRun]
-#file speciying experiment name and parameters
+##file speciying experiment name and parameters
 spec=path+'/'+experiments['specificationFile'][experimentToRun]
-#read the specification file, which has the parameter sets
+##read the specification file, which has the parameter sets
 #and the name of the base parameter file. which will be modified for runs
 thisExperiment=pd.read_csv(spec)
-#get the parameter file name
+##get the parameter file name
 baseParameterFile=path+'/'+thisExperiment['baseParameterFile'][0] 
 
 #read the parameter file that forms the basis for the experiment
 with open(baseParameterFile, 'r') as reader:
     #read in the entire file to a list, one element per line
     # Note: readlines doesn't trim the line endings - so need to include \n
+    ##list of parameters
     parameters = reader.readlines()
 
-#function find and change a given parameter
 def change(old,new):
+    '''
+    function find and change a given parameter
+    '''
     a=next((s for s in parameters if s.startswith(old)), 'missing')
     if a!='missing':
         ik=parameters.index(a)
@@ -78,14 +81,17 @@ def change(old,new):
 change("experiment.output.directory","experiment.output.directory="+path+"\n")
 change("experiment.name","experiment.name=runs\n")    
    
-#get the number of columns and rows for the parameters
+##get the number of columns 
 rows=thisExperiment.shape[0]
+##and rows for the parameters
 cols=thisExperiment.shape[1]
 #loop over the parameter sets, 1 per row
 for parameterSet in range(rows):
     #loop over the parameters to be changed from the base parameter file
     for m in range(cols-2):
+        ##get the parameter name
         param=thisExperiment.columns[m+2]
+        ##make sure the new value is a string
         newValue=str(thisExperiment.loc[parameterSet,param])
         #change parameters - note need to give whole line including termination
         change(param,param+"="+newValue+"\n")
@@ -97,12 +103,12 @@ for parameterSet in range(rows):
             # Write out the whole parameter file in one go
             writer.writelines(parameters)
         #Directly run the model!
-        #format a current time string
+        ##format a current time string
         now=datetime.now().strftime("%d-%m-%Y_%H:%M:%S")
         #output the model trace to a file called RunFile with the current date/time
         #add the parameterSet number in case the time is the same!
         print("Running model set:"+str(parameterSet))
-        os.system(root+execname+" temporaryParameterFile >"+path+"/RunFile"+str(parameterSet)+"_"+now)
+        os.system(root+execname+" temporaryParameterFile >"+path+"/RunFiles/RunFile"+str(parameterSet)+"_"+now)
         print("Removing temporary parameter file")
         os.system("rm temporaryParameterFile")
 
