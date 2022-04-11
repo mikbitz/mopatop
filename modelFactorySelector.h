7540f8d6c6990387cb6f2faff84238905dd1d90b
Mike <mb425@cam.ac.uk>
2022-04-11 15:25:39 +0100

    updates for model factory selector docs

--- modelFactorySelector.h
+++ modelFactorySelector.h
@@ -1,5 +1,30 @@
 #ifndef MODELFACTORYSELECTOR_H
 #define MODELFACTORYSELECTOR_H
+/* A program to model agents moving between places
+    Copyright (C) 2021  Mike Bithell
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    */
+
+//------------------------------------------------------------------------
+//------------------------------------------------------------------------
+/**
+ * @file modelFactorySelector.h 
+ * @brief File containing the definition of the selector for model factories
+ * 
+ * @author Mike Bithell
+ * @date 11/04/2022
+ **/
 class modelFactory;
 #include <string>
 /** @brief A class to pick one of a number of possible agent factories 
